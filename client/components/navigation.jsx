1feaafbf863707481e945fa4cc834f51d4da2a0d
Sarah Yu <sarah.m.yu88@gmail.com>
2015-11-10 12:28:17 -0800

    Add eslint config file and tweak navigation styles

--- client/components/navigation.jsx
+++ client/components/navigation.jsx
@@ -13,7 +13,7 @@ export default class Navigation extends React.Component {
     this.setState({sidebarOpen: !this.state.sidebarOpen});
   }
   render() {
-    let sidebarContent = <a className="item">Test content</a>
+    let sidebarContent = <div className="item react-sidebar">Test content</div>
 
     return (
       <div>
@@ -21,17 +21,23 @@ export default class Navigation extends React.Component {
           <div className="item react-index-top" onClick={this.toggleSidebar}>
             <i className="sidebar icon"></i>
           </div>
-          <a className="ui right item react-index-top">KQED LP</a>
+          <a className="item react-index-top">KQED LP</a>
           <div className="ui right item react-index-top">
             <i className="user icon"></i>
             Log in
           </div>
+          <div className="ui right item">
+            <div className="ui icon input">
+              <input type="text" placeholder="Search..." />
+              <i className="search link icon"></i>
+            </div>
+          </div>
         </div>
-        <Sidebar sidebar={sidebarContent}
-          open={this.state.sidebarOpen}
-          onSetOpen={this.toggleSidebar}>
-          <div></div>
-        </Sidebar>
+          <Sidebar sidebar={sidebarContent}
+            open={this.state.sidebarOpen}
+            onSetOpen={this.toggleSidebar}>
+            <div></div>
+          </Sidebar>
       </div>
     );
   }
